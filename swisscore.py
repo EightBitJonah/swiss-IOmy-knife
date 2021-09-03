3e324544da45b7241d9f421b793292432e801ffd
EightBitJonah <89947257+EightBitJonah@users.noreply.github.com>
2021-09-03 16:14:02 -0400

    Add files via upload

--- swisscore.py
+++ swisscore.py
@@ -1,21 +1,20 @@
 import subprocess,sys,time,logging,os
+import modules.nmapper
 
-print("Loading SwissCore . . . ")
- 
+#func for the swissmenu
 def swissmenu() :
         print("What would you like to launch today? \n (Please enter a number) \n 1 - nmapper \n 2 - Coming Soon! \n 3 - Exit")
         moduleload = input ("> ")
-        while True :
-
-                if moduleload == "1" :
-                        import nmapper.nmapper 
-                elif moduleload == "2" :
-                        print("There is no module to load. Come back later!")
-                        swissmenu()
-                elif moduleload == "3" : 
-                        print("Goodbye!")
-                        time.sleep(3)
-                        exit()
-                elif moduleload != ["1","2","3"] :
-                        swissmenu()
+        if moduleload == "1" :
+                modules.nmapper.nmapper()
+                swissmenu()
+        elif moduleload == "2" :
+                print("There is no module to load. Come back later!")
+                swissmenu()
+        elif moduleload == "3" : 
+                print("Goodbye!")
+                time.sleep(3)
+                exit()
+        elif moduleload != ["1","2","3"] :
+                swissmenu()
 swissmenu()
