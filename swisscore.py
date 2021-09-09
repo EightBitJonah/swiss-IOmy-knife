5de480de6e5124f571429d28f898a5340a546760
Austin Lucas <aclucas196@gmail.com>
2021-09-09 11:15:11 -0400

    Update Menu Loop

--- swisscore.py
+++ swisscore.py
@@ -1,20 +1,22 @@
 import subprocess,sys,time,logging,os
 import nmapper.nmapper
 
+menu_str = "What would you like to launch today? \n (Please enter a number) \n 1 - nmapper \n 2 - Coming Soon! \n 3 - Exit"
+
 #func for the swissmenu
 def swissmenu() :
-        print("What would you like to launch today? \n (Please enter a number) \n 1 - nmapper \n 2 - Coming Soon! \n 3 - Exit")
+    in_menu = True
+    while in_menu:
+        print(menu_str)
         moduleload = input ("> ")
         if moduleload == "1" :
-                nmapper.nmapper.nmapper()
-                swissmenu()
+            nmapper.nmapper.nmapper()
         elif moduleload == "2" :
-                print("There is no module to load. Come back later!")
-                swissmenu()
+            print("There is no module to load. Come back later!")
         elif moduleload == "3" : 
-                print("Goodbye!")
-                time.sleep(3)
-                exit()
+            print("Goodbye!")
+            in_menu = False
         elif moduleload != ["1","2","3"] :
-                swissmenu()
+            print("Invalid value!")
+
 swissmenu()
