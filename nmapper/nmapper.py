022e8c5065ebeb4ae76dd52647a0fee11a9caa58
EightBitJonah <89947257+EightBitJonah@users.noreply.github.com>
2022-02-13 00:31:49 -0500

    Add files via upload
    Removed Windows support, fixed *nix support and reading from API Keys files

--- nmapper/nmapper.py
+++ nmapper/nmapper.py
@@ -3,19 +3,22 @@ import subprocess,time,logging,os,sys
 __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__))) 
 
+nmapperfolder = os.path.dirname(os.path.abspath(__file__))
+targetfile = os.path.join (nmapperfolder, 'targets.txt')
+
+
 # API Keys are defined in a txt file
 accessfile = open(os.path.join(__location__, '..','accesskey.txt'))
 accesskey = accessfile.read()
-accessfile.close()
 
-secretfile = open(os.path.join(__location__, '..','accesskey.txt'))
+
+secretfile = open(os.path.join(__location__, '..','secretkey.txt'))
 secretkey = secretfile.read()
-secretfile.close()
 
-#targetfile = open(os.path.join(__location__, 'targets.txt'))
+
 
 from tenable.io import TenableIO
-tio = TenableIO('{accesskey}','{secretkey}')
+tio = TenableIO(accesskey,secretkey)
 
 
 windowsuser = os.name == 'nt'
@@ -26,30 +29,28 @@ def nmapper() :
             print("This script does not support Windows, returning to SwissCore")
             time.sleep(3)
             return
-    
         
         scan = input("Please provide the scan ID that you would like to update: ")
-        targetdir = input("Enter destination for Targets file: " )
         targets = input("Please provide a CIDR range to scan: ")
         
 
-        print('Using these settings...' + '\n' + 'Scan ID ' + scan + '\n' + targetdir +'/targets.txt' + '\n'  'Targets ' + targets)
+        print('Using these settings...' + '\n' + 'Scan ID ' + scan + '\n' + targetfile + '\n'  'Targets ' + targets)
         print('Are these settings correct? Y/N')
         nmappernext = input('> ')
-        if nmappernext == ["Y", "y"] :
+        if nmappernext == ["Y"] :
             print('Starting nmap scan!')
-        elif nmappernext == ["N","n"] :
-            nmapper()
-
-
-                     
+            #settings_menu = False
+        elif nmappernext == ["N"] :
+            print("Returning to settings input...")
+            nmapper()           
 
         # Defining nmap scan + target output
-        NMAPPER = "nmap -n -sn " + targets + " -oG - | awk '/Up$/{print $2}' > " + targetdir + "/targets.txt"
-        #NMAPPERWIN = "nmap -n -sn " + targets + " -oG - | powershell ForEach-Object { if ($_ -match 'Host: (\S+)') { $Matches[1] } } > " + targetdir
+        NMAPPER = "nmap -n -sn " + targets + " -oG - | awk '/Up$/{print $2}' > " + targetfile 
+        NMAPPERWIN = "nmap -n -sn " + targets + " -oG - | powershell ForEach-Object { if ($_ -match 'Host: (\S+)') { $Matches[1] } } > " + targetfile 
 
         time.sleep(5)   
 
+        
 
         if nixuser :
             subprocess.call(NMAPPER, shell=True)
@@ -58,34 +59,36 @@ def nmapper() :
             else :
                 print("nmap is not installed")
                 exit()
-        #elif windowsuser : 
-        #    subprocess.run(NMAPPERWIN, shell=True)
-        #    if "Nmap done" :
-        #        print('nmap scan complete, reading target list...')
-        #    else :
-        #        print("nmap is not installed")
+        elif windowsuser : 
+            result = subprocess.Popen([NMAPPERWIN], stdout=subprocess.PIPE, shell=True)
+            result.stdout
+            if "Nmap done" :
+                print('nmap scan complete, reading target list...')
+            else :
+                print("nmap is not installed")
+                exit()
 
-
+        print(f"Reading targets from {targetfile}...")
         #defining target list to read
-        mytargets = open(targetdir,"r")
+        mytargets = open(targetfile, 'r')
         content = mytargets.read()
-        content_list = content.split(",")
+        content_list = content.split(',')
         mytargets.close()
 
         print('Adding targets to scan...')
 
         #update scan
         tio.scans.configure(scan,
-        targets=content_list
-        )
+        targets=content_list)
+        
 
 
         print('Success!')
         print('Would you like to run another nmap scan? Y/N')
         nmappernext
-        if nmappernext == ["Y","n"] :
+        if nmappernext == ["Y"] :
             nmapper()
-        elif nmappernext == ["N","n"] :
+        elif nmappernext == ["N"] :
             return
 
 
@@ -93,5 +96,5 @@ def nmapper() :
 
 
 
-if __name__ == "__nmapper__" : 
-        nmapper()
+#if __name__ == "__nmapper__" : 
+#        nmapper()
