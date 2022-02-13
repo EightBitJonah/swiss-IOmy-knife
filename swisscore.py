bf5720ed1627dba858da7d6261731a12d904acf2
EightBitJonah <89947257+EightBitJonah@users.noreply.github.com>
2022-02-13 01:44:44 -0500

    Add files via upload

--- swisscore.py
+++ swisscore.py
@@ -1,7 +1,8 @@
-import subprocess,sys,time,logging,os
+import subprocess,sys,time,logging,os,pprint
 import nmapper.nmapper
+import mescans.mescans
 
-menu_str = "What would you like to launch today? \n (Please enter a number) \n 1 - nmapper \n 2 - Coming Soon! \n 3 - Exit"
+menu_str = "What would you like to launch today? \n (Please enter a number) \n 1 - nmapper \n 2 - mescans \n 3 - help (coming soon) \n 4 - Exit"
 
 #func for the swissmenu
 def swissmenu() :
@@ -12,11 +13,13 @@ def swissmenu() :
         if moduleload == "1" :
             nmapper.nmapper.nmapper()
         elif moduleload == "2" :
-            print("There is no module to load. Come back later!")
+            mescans.mescans.mescans()
         elif moduleload == "3" : 
+            print("Under construction!")
+        elif moduleload == "4" :
             print("Goodbye!")
             in_menu = False
-        elif moduleload != ["1","2","3"] :
+        elif moduleload != ["1","2","3","4"] :
             print("Invalid value!")
 
 swissmenu()
